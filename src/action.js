61133ee6b501bfd863a14fc017637c4b82607c7a
shadrach akaade <akaadesm@gmail.com>
2022-09-02 08:08:32 +0100

    query api for games by genre

--- src/action.js
+++ src/action.js
@@ -1,29 +1,27 @@
-export const getGames = () => {
-    fetch('https://api.rawg.io/api/games?key=6e3446bcac6d4b9b9e33e75472a09c4f').then(function(response){
+export const gamesAction = () => {
+    fetch('https://api.rawg.io/api/games?key=6e3446bcac6d4b9b9e33e75472a09c4f&genres=4').then(function(response){
     response.json().then(function(data) {
         console.log(data.results);
-        displayGames(data.results);
+        showActionGames(data.results);
     });
 }).catch(function(error) {
     console.log('Fetch Error:', error);
 });
 }
 
-export const displayGames = (data) => {
+export const showActionGames = (data) => {
     const featured = data.map(game => {
         return `
-        <div class="col-md-3 allgames py-3 m-auto">
+        <div class="col-md-3 actgames py-3 m-auto">
             <div class="card border-danger w-100">
         <img src="${game.background_image}" class="card-img-top" alt="...">
         <div class="card-body bg">
           <h5 class="card-title">${game.name}</h5>
-
           
         </div>
         </div>
         </div>
         `
     })
-    document.getElementById('category').insertAdjacentHTML("afterbegin", featured);
+    document.getElementById('category2').insertAdjacentHTML("afterbegin", featured);
 }
-
--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 import './styles/style.scss';
 import { getGames } from './games';
+import { gamesAction } from './action';
+import { racingGames } from './racing';
+import { sportGames } from './sports';
 
 getGames();
 
-const actionGames = document.getElementById('Action');
+const saction = document.getElementById('gaction');
+const actiongames = document.getElementById('category2');
+const tgames = document.querySelector('.actgames');
+const wrapper = document.getElementById('category');
+const racingbtn = document.getElementById('racing');
+const racinggames = document.getElementById('category3');
+const sportsbtn = document.getElementById('sports');
+const sportgames = document.getElementById('category4')
+const homebtn = document.getElementById('home');
 
-//actionGames.addEventListener('click', () => {
-    //getGames.
-//})
+
+
+
+saction.addEventListener('click', () => {
+wrapper.style.display = 'none';
+gamesAction();
+
+});
+
+racingbtn.addEventListener('click', () => {
+    wrapper.style.display = 'none';
+    actiongames.style.display = 'none';
+    racingGames();
+} )
+
+sportsbtn.addEventListener('click', () => {
+    wrapper.style.display = 'none';
+    actiongames.style.display = 'none';
+    racinggames.style.display = 'none';
+    sportGames();
+})
+
+
